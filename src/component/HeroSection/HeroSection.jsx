f1208841aec8c54e04550de5e898a15a51374eb7
Krish_Na <krishnapan91@gmail.com>
2024-01-12 12:42:04 +0530

    completed herosection without animation

--- src/component/HeroSection/HeroSection.jsx
+++ src/component/HeroSection/HeroSection.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const HeroSection = () => {
   return (
-    <section className='w-full h-screen'>
-
+    <section className="w-full h-screen ">
+      <div className="w-3/4 md:ml-[3vw] ml-[8vw] md:mt-[4vw] mt-[30vh] flex flex-col gap-[5vh]">
+        <h1 className="md:text-[9vw] text-[15vw] md:text-gray-600 text-white flex flex-col leading-none">
+          <span>Wix Studio</span>
+          <span>Deliver brilliance.</span>
+          <span>Smash deadlines.</span>
+        </h1>
+        <button className="bg-white text-black rounded-3xl md:w-[20vw] w-[45vw] h-[7vh] text-xl">
+          Start creating <i className="fa-light fa-arrow-right"></i>
+        </button>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default HeroSection
+export default HeroSection;
